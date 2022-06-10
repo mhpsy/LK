44940262fcb681eee9254b07e5bb5ee3e667ae70
mhpsy <82896623+mhpsy@users.noreply.github.com>
2022-06-10 13:49:42 +0800

    35

---
+++ 35/searchInsert.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int r= nums.length -1;
+        int mid = 0;
+        while(l<=r){
+            mid = l+(r-l)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                r = mid -1;
+            }else{
+                l = mid +1;
+            }
+        }
+        return l;
+        //判断条件为l<=r也就是退出循环的条件为l>r也就是正确的下标位置
+    }
+}
