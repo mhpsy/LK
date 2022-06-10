ba99806eb9626cb31b12e60d9d2d01eb5fc7b9aa
mhpsy <82896623+mhpsy@users.noreply.github.com>
2022-06-10 14:18:38 +0800

    1385

---
+++ findTheDistanceValue.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        int ans = 0;
+        for(int a1 : arr1){
+            boolean flag = true;
+            for(int a2 :arr2){
+                if(Math.abs(a1-a2)<=d){
+                    flag = false;
+                }
+            }
+            if(flag){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
+//上面为一分钟写的暴力解法，可能以后会写一个二分（我也不清楚）
+
+
+
